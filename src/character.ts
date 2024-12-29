9f0da5a39ee006eb26432182ab258a5e0ee68598
Jose Roberts <webterpr@gmail.com>
2024-12-28 21:15:19 -0500

    update imports in character file

--- src/character.ts
+++ src/character.ts
@@ -1,4 +1,4 @@
-import { Character, ModelProviderName, defaultCharacter } from "@ai16z/eliza";
+import { Character, ModelProviderName, defaultCharacter } from "@elizaos/core";
 
 export const character: Character = {
     ...defaultCharacter,
